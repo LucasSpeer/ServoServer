2e1178e1d4652b724e7e0f877d9e5963b91fff25
lucas speer <lucasspeer123@gmail.com>
2022-09-23 13:22:41 -0500

    Moved web into main dir

--- m_server.py
+++ m_server.py
@@ -1,22 +1,36 @@
-from http.server import HTTPServer, CGIHTTPRequestHandler
-import urllib, json, time
+from http.server import HTTPServer, CGIHTTPRequestHandler, SimpleHTTPRequestHandler
+import urllib, json, time, curses
 import RPi.GPIO as GPIO
 
 sHandler = ''
 
 class MotorRequestHandler(CGIHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('content-type','text/html')
-        self.end_headers()
-        self.wfile.write('Python HTTP Webserver Tutorial'.encode())
+        #self.send_response(200)
+        #self.send_header('content-type','text/html')
+        #self.end_headers()
+        #if self.path == '/':
+        #    self.path = '/web/'
+        return SimpleHTTPRequestHandler.do_GET(self)
+        #self.wfile.write('Python HTTP Webserver Tutorial'.encode())
     
     def do_POST(self):
+        print(self.headers)
         length = int(self.headers['Content-Length'])
-        post_data = urllib.parse.parse_qs(self.rfile.read(length).decode('utf-8'))
+        d = self.rfile.read(length).decode('utf-8')
+        print('d:')
+        print(d)
+        post_data = urllib.parse.parse_qs(d)
         #print(post_data)
         handlePostData(post_data)
-        self.send_response(200)
+        self.send_response(200, "ok")
+        self.end_headers()
+        
+    def do_OPTIONS(self):           
+        self.send_response(200, "ok")       
+        self.send_header('Access-Control-Allow-Origin', '*')                
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        self.send_header("Access-Control-Allow-Headers", "*")        
         self.end_headers()
     
 def setDuty(d):
@@ -46,23 +60,51 @@ def setupMotors():
 def testLoop():
     global sHandler
     pwm = 0;
-    while pwm < 40:
+    while pwm < 25:
         setDuty(pwm)
-        pwm = pwm + 2;
+        pwm = pwm + 1;
     
 def handlePostData(post_data):
     global sHandler
+    print('pd:\n')
+    print(post_data)
     data = post_data["payload"]
+    print('d:')
     print(data)
     for x in data:
         jx = json.loads(x)
         for key in jx.keys():
             if key == 'cmd':
                 handleCommand(jx[key], float(jx['dur']))
+        
+
+def keyListen(stdscr):
+    # do not wait for input when calling getch
+    stdscr.nodelay(1)
+    while True:
+        # get keyboard input, returns -1 if none available
+        c = stdscr.getch()
+        if c != -1:
             
+            if c == 259:
+                handleCommand('up', 0.08)
+                stdscr.addstr('up   ')
+            elif c == 258:
+                handleCommand('dn', 0.08)
+                stdscr.addstr('down ')
+            else:
+                # print numeric value
+                stdscr.addstr(str(c) + ' ')
+                
+            stdscr.refresh()
+            # return curser to start position
+            stdscr.move(0, 0)
 
 def main():
+    
     setupMotors()
+    
+    #curses.wrapper(keyListen)
     
     PRT = 8110
     serv = HTTPServer(('',PRT), MotorRequestHandler)
@@ -70,5 +112,5 @@ def main():
     serv.serve_forever()
     
 if __name__ == '__main__':
-    print('Starting')
+    #print('Starting')
     main()
